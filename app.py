1563e54a7fcfbf9733e9d4625d2e85a84f99f333
ablazleon <ablazleon@gmail.com>
2020-01-02 03:32:18 -0800

    UPdate to a post and a get

--- app.py
+++ app.py
@@ -2,49 +2,40 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
+@app.route('/location/cars/', methods=['GET'])
 def respond():
     # Retrieve the name from url parameter
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"got name {name}")
-
+    emptySlots = 1
     response = {}
 
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+    # For debugging
+    print(f" {emptySlots} free slot(s)")
+
+    response["MESSAGE"] = f"{emptySlots} free slot(s)"
 
     # Return the response in json format
     return jsonify(response)
 
-@app.route('/post/', methods=['POST'])
+@app.route('/location/cars/', methods=['POST'])
 def post_something():
-    param = request.form.get('name')
+    param = request.form.get('image')
     print(param)
     # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
     if param:
         return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
+            "Message": f"Image sent for processing",
             # Add this option to distinct the POST request
             "METHOD" : "POST"
         })
     else:
         return jsonify({
-            "ERROR": "no name found, please send a name."
+            "ERROR": "no image found, please send a name."
         })
 
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    return "<h1> Welcome to Gorrilla !!</h1>"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
